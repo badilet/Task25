d0f54b9f8514f9f19e584ccedc42ea5ea4b4b018
User <user@Users-MacBook-Pro-235.local>
2019-12-29 21:24:21 -0600

    first commit

--- Task25.py
+++ Task25.py
@@ -1,12 +1,11 @@
 # Напишите функцию которая находит самое длинное слово в строке.
-# sentence = input("Enter a sentence:")
-# def longest_word():
-# print(len(sentence.split()))
-def find_longest_word(word_list):
-    longest_word = ''
-    for word in word_list:
-        if len(word) > len(longest_word):
-            longest_word = word
-            print(longest_word)
 
-find_longest_word()
+def myfunc():
+    text = input("Enter a sentence: ").split(" ")
+    count = 0
+    for i in text:
+        if len(i) > count:
+            count = len(i)
+            word = i
+    print("The longest word is: ", word)
+myfunc()
